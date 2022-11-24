3b585ed848cdea8c45ae26cb77e4aa9e080353bc
b3atz <c4werth@outlook.com>
2022-11-24 10:41:42 -0800

    Set up sections, nav bar, and layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,6 +7,7 @@
 
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 import Header from "./header"
 import "./layout.css"
@@ -41,7 +42,9 @@ const Layout = ({ children }) => {
         >
           © {new Date().getFullYear()} &middot; Built with
           {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          <a href="https://www.gatsbyjs.com">
+          <StaticImage src="../images/gatsby-icon.png"  class="imgfooter"/>
+          </a>
         </footer>
       </div>
     </>
